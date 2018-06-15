ce9872d89ce2986afb574607c5bb9017913cfd88
Gustavo de Castro Biage <gustavo.c.biage@gmail.com>
2018-06-15 16:34:28 -0300

    New Point Class in Demo.java File

--- src/Demo.java
+++ src/Demo.java
@@ -4,3 +4,25 @@ public class Demo {
     	
     }
 }
+
+class Point
+{
+	public int x, y;
+	
+	public Point(int x, int y) {
+		this.y = y;
+		this.x = x;
+	}
+	
+	public int x() {
+		return x;
+	}
+	
+	public int y() {
+		return y;
+	}
+	
+	
+	
+	
+}
